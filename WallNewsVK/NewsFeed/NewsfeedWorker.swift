253bc7130e34d5fcb7d939fc32fbfe0f2a0b9d34
Mishel <mir89@mail.ru>
2021-01-14 17:48:49 +0400

    new

---
+++ WallNewsVK/NewsFeed/NewsfeedWorker.swift
@@ -0,0 +1,13 @@
+//
+//  NewsfeedWorker.swift
+//  WallNewsVK
+//
+//  Created by User on 08.01.2021.
+//  Copyright (c) 2021 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class NewsfeedService {
+
+}
